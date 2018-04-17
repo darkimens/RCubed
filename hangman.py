50e6be1a3c1461911260610d3b6a6a7b32b62c95
Rishab Jain <397630@bsd48.org>
2018-04-16 19:44:54 -0700

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -1,88 +1,220 @@
-#importing the time module
-import time
+"""Hangman
+Standard game of Hangman. A word is chosen at random from a list and the
+user must guess the word letter by letter before running out of attempts."""
 
-#welcoming the user
-name = raw_input("What is your name? ")
-
-print "Hello, " + name, "it's time to play hangman!"
-
-print ""
-
-#wait for 1 second
-time.sleep(1)
-
-print "Start guessing..."
-time.sleep(0.5)
-
-#generates random word
 import random
 
-WORDS = ("python", "jumble", "easy", "difficult", "answer",  "xylophone", "fish", "cube", "device")
-word = random.choice(WORDS)
-#creates an variable with an empty value
-guesses = ''
+def main():
+    welcome = ['Welcome to Hangman! A word will be chosen at random and',
+               'you must try to guess the word correctly letter by letter',
+               'before you run out of attempts. Good luck!'
+               ]
 
-#determine the number of turns
-turns = 10
+    for line in welcome:
+        print(line, sep='\n')
 
-# Create a while loop
+    # setting up the play_again loop
 
-#check if the turns are more than zero
-while turns > 0:         
+    play_again = True
 
-    # make a counter that starts with zero
-    failed = 0             
+    while play_again:
+        # set up the game loop
 
-    # for every character in secret_word    
-    for char in word:      
+        words = ["hangman", "chairs", "backpack", "bodywash", "clothing",
+                 "computer", "python", "program", "glasses", "sweatshirt",
+                 "sweatpants", "mattress", "friends", "clocks", "biology",
+                 "algebra", "suitcase", "knives", "ninjas", "shampoo"
+                 ]
 
-    # see if the character is in the players guess
-        if char in guesses:    
-    
-        # print then out the character
-            print char,    
+        chosen_word = random.choice(words).lower()
+        player_guess = None # will hold the players guess
+        guessed_letters = [] # a list of letters guessed so far
+        word_guessed = []
+        for letter in chosen_word:
+            word_guessed.append("-") # create an unguessed, blank version of the word
+        joined_word = None # joins the words in the list word_guessed
 
-        else:
-    
-        # if not found, print a dash
-            print "_",     
+        HANGMAN = (
+"""
+-----
+|   |
+|
+|
+|
+|
+|
+|
+|
+--------
+""",
+"""
+-----
+|   |
+|   0
+|
+|
+|
+|
+|
+|
+--------
+""",
+"""
+-----
+|   |
+|   0
+|  -+-
+|
+|
+|
+|
+|
+--------
+""",
+"""
+-----
+|   |
+|   0
+| /-+-
+|
+|
+|
+|
+|
+--------
+""",
+"""
+-----
+|   |
+|   0
+| /-+-\ 
+|
+|
+|
+|
+|
+--------
+""",
+"""
+-----
+|   |
+|   0
+| /-+-\ 
+|   | 
+|
+|
+|
+|
+--------
+""",
+"""
+-----
+|   |
+|   0
+| /-+-\ 
+|   | 
+|   | 
+|
+|
+|
+--------
+""",
+"""
+-----
+|   |
+|   0
+| /-+-\ 
+|   | 
+|   | 
+|  |
+|
+|
+--------
+""",
+"""
+-----
+|   |
+|   0
+| /-+-\ 
+|   | 
+|   | 
+|  | 
+|  | 
+|
+--------
+""",
+"""
+-----
+|   |
+|   0
+| /-+-\ 
+|   | 
+|   | 
+|  | | 
+|  | 
+|
+--------
+""",
+"""
+-----
+|   |
+|   0
+| /-+-\ 
+|   | 
+|   | 
+|  | | 
+|  | | 
+|
+--------
+""")
 
-        # and increase the failed counter with one
-            failed += 1    
+        print(HANGMAN[0])
+        attempts = len(HANGMAN) - 1
 
-    # if failed is equal to zero
 
-    # print You Won
-    if failed == 0:        
-        print "You won"  
+        while (attempts != 0 and "-" in word_guessed):
+            print(("\nYou have {} attempts remaining").format(attempts))
+            joined_word = "".join(word_guessed)
+            print(joined_word)
 
-    # exit the script
-        break              
+            try:
+                player_guess = str(input("\nPlease select a letter between A-Z" + "\n> ")).lower()
+            except: # check valid input
+                print("That is not valid input. Please try again.")
+                continue                
+            else: 
+                if not player_guess.isalpha(): # check the input is a letter. Also checks an input has been made.
+                    print("That is not a letter. Please try again.")
+                    continue
+                elif len(player_guess) > 1: # check the input is only one letter
+                    print("That is more than one letter. Please try again.")
+                    continue
+                elif player_guess in guessed_letters: # check it letter hasn't been guessed already
+                    print("You have already guessed that letter. Please try again.")
+                    continue
+                else:
+                    pass
 
-    print
+            guessed_letters.append(player_guess)
 
-    # ask the user to guess a character
-    guess = raw_input("Guess a character:") 
+            for letter in range(len(chosen_word)):
+                if player_guess == chosen_word[letter]:
+                    word_guessed[letter] = player_guess # replace all letters in the chosen word that match the players guess
 
-    # set the players guess to guesses
-    guesses += guess                    
+            if player_guess not in chosen_word:
+                attempts -= 1
+                print(HANGMAN[(len(HANGMAN) - 1) - attempts])
 
-    # if the guess is not found in the secret word
-    if guess not in word:  
- 
-     # turns counter decreases with 1 (now 9)
-        turns -= 1        
- 
-    # print wrong
-        print "Oh no! That's Wrong"    
- 
-    # how many turns are left
-        print "You have", + turns, 'more guesses' 
- 
-    # if the turns are equal to zero
-        if turns == 0:           
-    
-        # print "You Lost"
-            print "You Lost, :("
-            print"The word was:"
-            print(word)
+        if "-" not in word_guessed: # no blanks remaining
+            print(("\nCongratulations! {} was the word").format(chosen_word))
+        else: # loop must have ended because attempts reached 0
+            print(("\nUnlucky! The word was {}.").format(chosen_word))
+
+        print("\nWould you like to play again?")
+
+        response = input("> ").lower()
+        if response not in ("yes", "y"):
+            play_again = False
+
+if __name__ == "__main__":
+    main()
